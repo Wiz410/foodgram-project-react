0ba8230c8ef18ef439091d0a59828182bc59c477
Данила Полунин <d.polunin2001@gmail.com>
2023-12-18 17:05:36 +0300

    Started from the beginning

--- backend/api/v1/paginations.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class RecipeAndSubscriptionPagination(PageNumberPagination):
-    """Пагинация для рецептов и подписок."""
-    page_size_query_param = 'limit'
--- backend/start_migrate_collectstatic.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sleep 10
-python manage.py migrate
-python manage.py collectstatic
-cp -r /app/collected_static/. /static/
-gunicorn --bind 0.0.0.0:8000 foodgram.wsgi
