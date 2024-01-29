b31f9d6f68f8d1d9656d979681e724f37f54c228
Данила Полунин <d.polunin2001@gmail.com>
2024-01-29 12:19:45 +0300

    Test actiom 9.

--- backend/foodgram_start.sh
+++ backend/foodgram_start.sh
@@ -3,6 +3,6 @@
 sleep 10
 python manage.py migrate
 python manage.py foodgram_import
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 cp -r /app/collected_static/. /staticfiles/
-gunicorn --bind 0.0.0.0:8000 foodgram.wsgi
+gunicorn --bind 0.0.0.0:8005 foodgram.wsgi
